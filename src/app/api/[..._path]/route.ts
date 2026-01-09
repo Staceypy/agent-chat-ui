73bebf8b0e816ebe7aa787b444a38079446a3b92
Yu Pei <13625410672@163.com>
2026-01-09 19:49:35 +0800

    separater api key

--- src/app/api/[..._path]/route.ts
+++ src/app/api/[..._path]/route.ts
@@ -8,6 +8,8 @@ export const { GET, POST, PUT, PATCH, DELETE, OPTIONS, runtime } =
     apiUrl: process.env.VERCEL_ENV === "production"
       ? process.env.LANGGRAPH_API_URL_PROD ?? "remove-me"
       : process.env.LANGGRAPH_API_URL_DEV ?? "remove-me",
-    apiKey: process.env.LANGSMITH_API_KEY ?? "remove-me", // default, if not defined it will attempt to read process.env.LANGSMITH_API_KEY
+    apiKey: process.env.VERCEL_ENV === "production"
+      ? process.env.LANGSMITH_API_KEY_PROD ?? "remove-me"
+      : process.env.LANGSMITH_API_KEY_DEV ?? "remove-me", // default, if not defined it will attempt to read process.env.LANGSMITH_API_KEY
     runtime: "edge", // default
   });

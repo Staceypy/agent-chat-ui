dd046f807b0fe808ec621aa5a72f11cf4f4e0de7
Yu Pei <13625410672@163.com>
2026-01-09 22:22:44 +0800

    revert to one langgraph url

--- src/app/api/[..._path]/route.ts
+++ src/app/api/[..._path]/route.ts
@@ -5,11 +5,7 @@ import { initApiPassthrough } from "langgraph-nextjs-api-passthrough";
 
 export const { GET, POST, PUT, PATCH, DELETE, OPTIONS, runtime } =
   initApiPassthrough({
-    apiUrl: process.env.VERCEL_ENV === "production"
-      ? process.env.LANGGRAPH_API_URL_PROD ?? "remove-me"
-      : process.env.LANGGRAPH_API_URL_DEV ?? "remove-me",
-    apiKey: process.env.VERCEL_ENV === "production"
-      ? process.env.LANGSMITH_API_KEY_PROD ?? "remove-me"
-      : process.env.LANGSMITH_API_KEY_DEV ?? "remove-me", // default, if not defined it will attempt to read process.env.LANGSMITH_API_KEY
+    apiUrl:  process.env.LANGGRAPH_API_URL ?? "remove-me",
+    apiKey: process.env.LANGSMITH_API_KEY ?? "remove-me", // default, if not defined it will attempt to read process.env.LANGSMITH_API_KEY
     runtime: "edge", // default
   });

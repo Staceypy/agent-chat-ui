3c6e3416962dd28e03e694071709bc1398206621
Yu Pei <13625410672@163.com>
2026-01-19 11:49:24 +0100

    separate dev and live langgraph utl

--- src/app/api/[..._path]/route.ts
+++ src/app/api/[..._path]/route.ts
@@ -5,7 +5,11 @@ import { initApiPassthrough } from "langgraph-nextjs-api-passthrough";
 
 export const { GET, POST, PUT, PATCH, DELETE, OPTIONS, runtime } =
   initApiPassthrough({
-    apiUrl:  process.env.LANGGRAPH_API_URL ?? "remove-me",
-    apiKey: process.env.LANGSMITH_API_KEY ?? "remove-me", // default, if not defined it will attempt to read process.env.LANGSMITH_API_KEY
+    apiUrl: process.env.VERCEL_ENV === "production"
+      ? process.env.LANGGRAPH_API_URL_PROD ?? "remove-me"
+      : process.env.LANGGRAPH_API_URL_DEV ?? "remove-me",
+    apiKey: process.env.VERCEL_ENV === "production"
+      ? process.env.LANGSMITH_API_KEY_PROD ?? "remove-me"
+      : process.env.LANGSMITH_API_KEY_DEV ?? "remove-me", // default, if not defined it will attempt to read process.env.LANGSMITH_API_KEY
     runtime: "edge", // default
   });
